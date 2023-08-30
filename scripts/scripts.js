b0cb61e64503fbaa52205ec7c457bb9dc199c2cf
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-08-30 20:11:56 +0600

    first commit

---
+++ scripts/scripts.js
@@ -0,0 +1,18 @@
+const loadCatagories = async() => {
+    const response = await fetch('https://openapi.programming-hero.com/api/news/categories');
+    const data = await response.json();
+    const categories = data.data.news_category;
+    console.log(categories);
+    const tabContainer = document.getElementById('tab-container');
+
+    categories.forEach(element => {
+        const tabDiv = document.createElement('div');
+    tabDiv.innerHTML = `
+  <a class="tab">${element.category_name}</a> 
+    `
+    tabContainer.appendChild(tabDiv);
+    });
+
+}
+
+loadCatagories();
